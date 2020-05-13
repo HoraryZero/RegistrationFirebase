8f741502ce7eebc993c794a73884eb4b0b81d547
horaryzero <rnd.programmer.mike@gmail.com>
2020-05-13 20:44:25 +0300

    Added Google authorization, state saving added

--- app/src/main/java/com/example/reg/HomeProfileActivity.kt
+++ app/src/main/java/com/example/reg/HomeProfileActivity.kt
@@ -1,5 +1,6 @@
 package com.example.reg
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
@@ -8,18 +9,25 @@ import kotlinx.android.synthetic.main.activity_home_profile.*
 class HomeProfileActivity : AppCompatActivity() {
 
     enum class ProviderType {
-        BASIC
+        BASIC,
+        GOOGLE
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_profile)
 
-        // Вход
         val bundle = intent.extras
         val email = bundle?.getString("email")
         val provider = bundle?.getString("provider")
         setup(email ?: "", provider ?: "")
+
+        // Сохранение состояния
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
+        prefs.apply()
+
     }
 
     private fun setup(email: String, provider: String) {
@@ -30,6 +38,12 @@ class HomeProfileActivity : AppCompatActivity() {
         providerTextView.text = provider
 
         logOutButton.setOnClickListener {
+
+            // Сохранение состояния
+            val prefs =  getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
             FirebaseAuth.getInstance().signOut()
             onBackPressed()
         }
